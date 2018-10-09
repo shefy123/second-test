a26cf4f460d6ab1a42170b671af22a3633c1aa31
shefy123 <38942966+shefy123@users.noreply.github.com>
2018-10-09 22:37:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// write your JS code here
+
+let alphabates = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+function initialize() {
+    for (let i = 0; i < alphabates.length; i++) {
+        document.querySelector('.card-body').innerHTML +=  `<div class="button-image">
+                                        <p id="letters">${alphabates[i]}</p>
+                                    </div>`
+                                     
+    }
+    document.querySelector('.card-body').innerHTML += ` <div class="space-bar" > <p>SPACE</p> </div>
+    <div class="massage-key" onclick="messageAlert()">
+    <p>
+        <i class="fas fa-arrow-circle-right"></i>
+    </p>
+    </div>`;
+
+}
+
+// alert message when you click arrow button
+
+function messageAlert() {
+    alert("message deliverd");
+}
+
